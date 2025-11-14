e2a4b53b85a414e26acaa2efd8dd72c5088eac10
igapiga2012 <igawarzych@gmail.com>
2025-11-14 12:26:10 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
-let waarde_kompas = 0
-waarde_kompas += 90
+let waarde_kompas = input.compassHeading()
 basic.forever(function () {
-    if (waarde_kompas > 44 && waarde_kompas < 136) {
+    if (waarde_kompas > 315 && waarde_kompas < 360 || waarde_kompas > 0 && waarde_kompas < 45) {
         basic.showLeds(`
             . . # . .
             . # # # .
@@ -10,30 +9,30 @@ basic.forever(function () {
             . . # . .
             `)
     }
-    if (waarde_kompas > 134 && waarde_kompas < 226) {
+    if (waarde_kompas < 136 && waarde_kompas > 46) {
         basic.showLeds(`
             . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
             . . # . .
             `)
     }
-    if (waarde_kompas > 316 && waarde_kompas < 224) {
+    if (waarde_kompas > 136 && waarde_kompas < 226) {
         basic.showLeds(`
             . . # . .
-            . # # # .
-            # . # . #
             . . # . .
+            # . # . #
+            . # # # .
             . . # . .
             `)
     }
-    if (waarde_kompas > 46 && waarde_kompas < 314) {
+    if (waarde_kompas > 226 && waarde_kompas < 316) {
         basic.showLeds(`
             . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
             . . # . .
             `)
     }
